ad51ba8aef8c8c318685f8ac0ff90358fc09d6de
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2020-08-15 15:27:40 +0200

    Added proper documentation

--- projectPortal/settings.py
+++ projectPortal/settings.py
@@ -78,6 +78,7 @@ REST_FRAMEWORK= {
     'DEFAULT_AUTHENTICATION_CLASSES': (
             'knox.auth.TokenAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 
--- projectPortal/urls.py
+++ projectPortal/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +23,5 @@ urlpatterns = [
     path('promo/',include('promo.api.urls')),
     path('professor/',include('professors.api.urls')),
     path('student/',include('students.api.urls')),
+    path('docs/',include_docs_urls(title="Project Portal"))
 ]
