6d3bb3fceb412cc5d5b14083715e6cfa331d882c
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2020-08-21 11:52:32 +0200

    Added Invite Model

--- students/models.py
+++ students/models.py
@@ -14,10 +14,20 @@ class Student(User):
     promo = models.ForeignKey(Promo,on_delete=models.CASCADE)
 
     note = models.IntegerField(null=True,blank=True)
-    #In case of student object deletion,we can't delete the whole team object
-    team = models.ForeignKey(Team,blank=True,null=True,on_delete=models.PROTECT)
+    #In case of team deletion,we can't delete the student
+    team = models.ForeignKey(Team,blank=True,null=True,on_delete=models.SET_NULL)
 
     is_leader = models.BooleanField(default=False,null=True,blank=True)
 
     def __str__(self):
         return self.last_name + ' ' + self.first_name
+
+class Invite(models.Model):
+    STATUS_CHOICES = (
+        ('P','Pending'),
+        ('A','Accepted'),
+        ('R','Rejected')
+    )
+    sender = models.ForeignKey(Student,on_delete=models.CASCADE,related_name='Sender')
+    receiver = models.ForeignKey(Student,on_delete=models.CASCADE,related_name='Receiver')
+    status = models.CharField(max_length=1,choices=STATUS_CHOICES)
