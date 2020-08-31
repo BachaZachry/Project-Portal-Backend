f0d0275af1b878bb0e3c2795efab993dc943100d
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2020-08-31 11:50:50 +0200

    Added Project Model

--- professors/models.py
+++ professors/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import User
+from promo.models import Promo
 
 class Professor(User):
     grades = (
@@ -17,3 +18,19 @@ class Professor(User):
             return 'Professor ' + self.last_name + ' ' + self.first_name
         else:
             return 'Dr.' + self.last_name + ' ' + self.first_name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=256)
+    domain = models.CharField(max_length=256)
+    professor = models.ForeignKey(Professor,on_delete=models.CASCADE)
+    tools = models.TextField()
+    required_documents = models.TextField()
+    document =models.FileField()
+    STATUS_CHOICES = (
+        ("A", "Accepted"),
+        ("P", "Pending"),
+        ("R", "Rejected"),
+    )
+    status = models.CharField(max_length=8,choices=STATUS_CHOICES)
+    promo = models.ForeignKey(Promo,on_delete=models.CASCADE)
