2c274c633d35b36f3c08a7a28d656d9ab46a470c
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2021-10-14 14:42:39 +0200

    Changing the password test

--- tests/test_user/e2e_tests.py
+++ tests/test_user/e2e_tests.py
@@ -1,9 +1,12 @@
 import factory
+from knox.models import AuthToken
 import pytest
+from rest_framework.test import APIClient
 from professors.models import Professor
 from students.models import Student
 
 from tests.factories import ProfessorFactory, StudentFactory
+from datetime import timedelta
 
 
 class TestUserEndpoints:
@@ -12,28 +15,43 @@ class TestUserEndpoints:
     @pytest.mark.django_db
     def test_login_student(self, api_client):
         student = StudentFactory()
-        print(Student.objects.all())
         expected_request_json = {
             'email': student.email,
             'password': 'pi3.1415'
         }
 
-        response = api_client().post(self.endpoint, data=expected_request_json, format='json')
-        print(response.json())
+        response = api_client().post(
+            '/users/login/', data=expected_request_json, format='json')
         assert response.status_code == 200
         assert response.json().get('Type') == 'student'
 
     @pytest.mark.django_db
     def test_login_professor(self, api_client):
         professor = ProfessorFactory()
-        print(Professor.objects.all())
         expected_request_json = {
             'email': professor.email,
             'password': 'pi3.1415'
         }
-        print(expected_request_json)
-        response = api_client().post(self.endpoint, data=expected_request_json,
+        response = api_client().post('/users/login/', data=expected_request_json,
                                      format="json")
-        print(response.json())
         assert response.status_code == 200
         assert response.json().get('Type') == 'professor'
+
+    @pytest.mark.django_db
+    def test_modify_password(self, api_client):
+        student = StudentFactory()
+        token = AuthToken.objects.create(user=student)[1]
+
+        current_pass = 'pi3.1415'
+        new_pass = 'newpassword234'
+        expected_request_json = {
+            'current_password': current_pass,
+            'new_password': new_pass
+        }
+        cli = api_client()
+        cli.credentials(HTTP_AUTHORIZATION='Token %s' % token)
+        response = cli.patch('/users/changepassword/',
+                             data=expected_request_json)
+        print(response)
+        print(response.json())
+        assert response.status_code == 200
